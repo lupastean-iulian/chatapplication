bb90381b53ed4f37f2b6c2f9a73c0f2b8c96a625
iulian-lupastean <iulian.lupastean@amdaris.com>
2023-01-22 15:01:13 +0200

    Create Project Structure

---
+++ ChatApplication.Dal/Data/ChatApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using ChatApplication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Dal.NewFolder
+{
+    public class ChatApplicationContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public ChatApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ChatApplication.Dal/EntityConfigurations/MessageEntityTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ChatApplication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Dal.EntityConfigurations
+{
+    public class MessageEntityTypeConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasMany(x => x.Replies)
+                .WithOne(x => x.Message);
+
+        }
+    }
+}
